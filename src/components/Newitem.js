e540b565c0d10ab11c70bef67cb9cfbe986bcb5e
Sandeep_kumar136 <Ksundip750#gmail.com>
2024-06-16 22:30:53 +0530

    Fatched API

--- src/components/Newitem.js
+++ src/components/Newitem.js
@@ -2,18 +2,18 @@ import React, { Component } from "react";
 
 export default class Newitem extends Component {
   render() {
-    let {title,discription}=this.props;
+    let {title,discription, imgUrl,newsURL}=this.props;
     return (
-      <div>
+      <div className="my-2">
         <div className="card m-1" style={{width: "18rem"}}>
-          <img src="..." className="card-img-top" alt="..." />
+          <img src={!imgUrl ? "https://c.ndtvimg.com/2024-06/u033l908_southwest-airlines_625x300_15_June_24.jpeg?ver-20240615.100": imgUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">
               {discription}
             </p>
-            <a href="/newsDetails" className="btn btn-primary">
-              Go somewhere
+            <a href={newsURL} target="blank" className="btn btn-sm btn-primary">
+              Get Details
             </a>
           </div>
         </div>
--- src/components/News.js
+++ src/components/News.js
@@ -2,63 +2,42 @@ import React, { Component } from "react";
 import Newitem from "./Newitem";
 
 export default class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+    };
+  }
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=7eed526b2ad547138ad720a811900fbe";
+
+    let data = await fetch(url);
+
+    let parsedData = await data.json();
+
+    this.setState({ articles: parsedData.articles });
+  }
   render() {
     return (
-      <div>
-        <div className="container">
-          <h2 className="text-center my-2 ">Top Headings</h2>
-          <div className="row">
-            <div className="col-md-3">
-              <Newitem
-                title="This is Top News"
-                discription="This is new discripition"
-              />
-            </div>
-            <div className="col-md-3">
-              <Newitem
-                title="This is Top News"
-                discription="This is new discripition"
-              />
-            </div>
-            <div className="col-md-3">
-              <Newitem
-                title="This is Top News"
-                discription="This is new discripition"
-              />
-            </div>
-            <div className="col-md-3">
-              <Newitem
-                title="This is Top News"
-                discription="This is new discripition"
-              />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-3">
-              <Newitem
-                title="This is Top News"
-                discription="This is new discripition"
-              />
-            </div>
-            <div className="col-md-3">
-              <Newitem
-                title="This is Top News"
-                discription="This is new discripition"
-              />
-            </div>
-            <div className="col-md-3">
-              <Newitem
-                title="This is Top News"
-                discription="This is new discripition"
-              />
-            </div>
-            <div className="col-md-3">
-              <Newitem
-                title="This is Top News"
-                discription="This is new discripition"
-              />
-            </div>
-          </div>
+      <div className="container">
+        <h2 className="text-center my-2 ">Top Headings</h2>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-3" key={element.url}>
+                <Newitem
+                  title={element.title ? element.title.slice(0, 44):""}
+                  discription={
+                    element.description ? element.description.slice(0, 87) : ""
+                  }
+                  imgUrl={element.urlToImage}
+                  newsURL={element.url}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
     );
